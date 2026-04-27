e75c68a43edbe0072c198a1e0ac78cea12d4ead1
Anvitha S <anvithas112@gamil.com>
2026-04-27 11:55:55 +0530

    gradle setup for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,20 +4,18 @@ plugins {
     id("maven-publish")
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "com.github.aiotrixdev"
-            artifactId = "art-kotlin-adk"
-            version = "1.0.0"
-
-            afterEvaluate {
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
                 from(components["release"])
+                groupId = "com.github.aiotrixdev"
+                artifactId = "art-kotlin-adk"
+                version = "1.0.0"
             }
         }
     }
 }
-
 android {
     namespace = "com.example.artlibrary"
     compileSdk = 35
@@ -85,4 +83,5 @@ dependencies {
 
     /* JSON */
     implementation("com.google.code.gson:gson:2.11.0")
+
 }
