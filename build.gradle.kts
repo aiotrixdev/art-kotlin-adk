4b65609f47c5bb0fc9d6bad61e11c65140f0723e
Anvitha S <anvithas112@gamil.com>
2026-04-28 16:07:15 +0530

    gradle setup for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("maven-publish")
 }
 
+val publishVersion = System.getenv("VERSION") ?: "1.0.0"
+
 afterEvaluate {
     publishing {
         publications {
@@ -11,7 +13,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.aiotrixdev"
                 artifactId = "art-kotlin-adk"
-                version = "1.0.0"
+                version = publishVersion
             }
         }
     }
